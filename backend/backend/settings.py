678883f2af2390a3753555160da5a8309cd7d279
Clébson Luiz <clebsonluiz5@gmail.com>
2023-07-11 22:07:25 -0300

    Correções no Docker e Docker Compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,8 +84,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_mysql_local',
+        'USER': 'django_user',
+        'PASSWORD': 'secret_password',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
